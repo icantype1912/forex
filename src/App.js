5d131548b76f29ff7e05025a1067913038ad7ae8
Adithya Jeyaram <adithya.jeyaram@gmail.com>
2024-06-12 17:03:29 +0530

    started off with API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React from "react";
 import "../src/App.css";
+import { useEffect,useState } from "react";
 
 
 
 
 const App = ()=>{
+
+  //states
+  const [start,setStart]  = useState(false);
+  const [toggle,setToggle] = useState(0);
+  const [amount,setAmount] = useState(null);
+  const [converted,setConverted] = useState(null);
+
+
+  useEffect(()=>{
+    if (start)
+      {
+        const host = 'api.frankfurter.app'
+        fetch(`https://${host}/latest?amount=${amount}&from=GBP&to=EUR`)
+        .then(resp => resp.json())
+        .then((data)=>{setConverted(data.rates.EUR)})
+      }
+    },[toggle])
+    
+  const Click = ()=>
+  {
+    setStart(true);
+    setToggle((prev) => prev+1)
+  }
+
+  const handleAmount = (e)=> {
+    setAmount(e.target.value);
+  }
+
+
+  
   return<div className="main-parent">
     <h2 className="header">Crimson Currency Converter</h2>
     <div className="table">
       <div className="converter">
         <div className="amount">
           <h4>Amount</h4>
-          <input type = "text"/>
+          <input type = "text" onSubmit={(e) => {e.preventDefault()}} onChange={handleAmount}/>
         </div>
         <div className="from">
           <h4>From</h4>
-          <select>
+          <select onSubmit={(e) => {e.preventDefault()}}>
             <option value = {from.short}>{from.long}</option>
           </select>
         </div>
@@ -24,19 +55,19 @@ const App = ()=>{
         </div>
         <div className="to">
           <h4>To</h4>
-          <select>
+          <select onSubmit={(e) => {e.preventDefault()}}>
             <option value = {to.short}>{to.long}</option>
           </select>
         </div>
       </div>
-      <div className="result">
+      {start?<div className="result">
         <p>{amount} {from.long} = </p>
         <h1>{converted} {to.long}</h1>
         <h5>1 {from.short} = {converted/amount} {to.short}</h5>
         <h5>1 {to.short} = {converted/amount} {from.short}</h5> 
-      </div>
+      </div>:<></>} 
       <div className="submit">
-        <button>Check</button>
+        <button onClick={Click}>Check</button>
       </div>
     </div>
   </div>
@@ -44,7 +75,5 @@ const App = ()=>{
 
 export default App;
 
-const amount = 10
 const from = {short:"GBP",long:"British Pounds"}
 const to = {short:"EUR",long:"Euros"}
-const converted = 20
