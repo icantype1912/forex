efae5ef813cea2e5cba8d0507e9c94a87e43e934
Adithya Jeyaram <adithya.jeyaram@gmail.com>
2024-06-14 15:52:56 +0530

    done(hopefully)

--- src/App.js
+++ src/App.js
@@ -1,114 +1,112 @@
 import React from "react";
 import "../src/App.css";
-import { useEffect,useState } from "react";
+import { useEffect, useState} from "react";
 import Converter from "./converter";
 import Result from "./result";
 import Submit from "./submit";
+import { debounce } from "lodash";
 
-
-const App = ()=>{
-
+const App = () => {
   //states
-  const [APIDone,setAPIDone] = useState(false)
-  const [start,setStart]  = useState(false);
-  const [amount,setAmount] = useState(1);
-  const [finalAmount,setFinalAmount] = useState(0)
-  const [finalFinalAmount,setFinalFinalAmount] = useState(0)
-  const [from,setFrom] = useState("USD");
-  const [finalFrom,setFinalFrom] = useState("USD")
-  const [finalFinalFrom,setFinalFinalFrom]  = useState("USD")
-  const [to,setTo] = useState("INR");
-  const [finalTo,setFinalTo] = useState("INR");
-  const [finalFinalTo,setFinalFinalTo]  = useState("USD")
-  const [converted,setConverted] = useState(0);
-  const [loading,setLoading] = useState(false);
+  const [APIDone, setAPIDone] = useState(true);
+  const [start, setStart] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [converted, setConverted] = useState(0);
+  const [amount, setAmount] = useState(1);
+  const[currency,setCurrency] = useState({from:"USD",to:"INR"})
 
 
-  useEffect(()=>{
-    if (start)
-      {
-        if(to===from)
-          {
-            alert("Can't convert to same currency!");
-            return
-          }
-          if((amount <= 0) || isNaN(amount))
-          {
-              return
-          }
-        setLoading(true)
-        const host = 'api.frankfurter.app'
-        fetch(`https://${host}/latest?amount=${finalAmount}&from=${from}&to=${to}`)
-        .then(resp => resp.json())
-        .then((data)=>{
-          setFinalFinalAmount(finalAmount)
-          setConverted(data.rates[to])
-          setLoading(false)
-          setFinalFinalFrom(finalFrom)
-          setFinalFinalTo(finalTo)
-          setStart(true);
-          setFinalAmount(amount);
-          setFinalTo(to);
-          setFinalFrom(from);
-          setAPIDone(true);
-        })
-      }
-    },[finalAmount,finalFrom,finalTo,from,start,to,amount])
-    
-  const Click = ()=>
-  {
-    if(to===from)
-      {
+  const debouncedSetAmount = 
+    debounce((value) => {
+      setAmount(value);
+    }, 50)
+
+  useEffect(() => {
+    if (start) {
+      setAPIDone(false)
+      if (currency.to === currency.from) {
         alert("Can't convert to same currency!");
-        return
+        return;
       }
-      if((amount <= 0) || isNaN(amount))
-      {
-          alert("Has to be a valid number greater than 0")
-          return
+      if (amount <= 0 || isNaN(amount)) {
+        return;
       }
-    setStart(true);
-    setFinalAmount(amount);
-    setFinalTo(to);
-    setFinalFrom(from);
-  }
-
-  const swap = ()=>
-    {
-      let temp = from;
-      setFrom(to);
-      setTo(temp);
+      setLoading(true);
+      const host = "api.frankfurter.app";
+      fetch(
+        `https://${host}/latest?amount=${amount}&from=${currency.from}&to=${currency.to}`
+      )
+        .then((resp) => resp.json())
+        .then((data) => {
+          setConverted(data.rates[currency.to]);
+          setLoading(false);
+          setStart(true);
+          setAPIDone(true);
+        });
     }
+  }, [currency.from, start, currency.to, amount]);
 
-  const handleAmount = (e)=> {
-    if(isNaN(e.target.value) && e.target.value !== "")
-      {
-        setAmount("")
-        return
-      }
-    setAmount(e.target.value);
-  }
+  const Click = () => {
+    if (currency.to === currency.from) {
+      alert("Can't convert to same currency!");
+      return;
+    }
+    if (amount <= 0 || isNaN(amount)) {
+      alert("Has to be a valid number greater than 0");
+      return;
+    }
+    setStart(true);
+  };
+
+  const swap = () => {
+    setCurrency((prevState)=>{return {to:prevState.from,from:prevState.to}})
+  };
+
+  const handleAmount = (e) => {
+    if (isNaN(e.target.value)) {
+      debouncedSetAmount(0);
+      return;
+    } 
+    debouncedSetAmount(e.target.value);
+  };
 
   const handleFrom = (e) => {
-    setFrom(e.target.value);
-  }
+    
+    setCurrency((prevState)=>{return {to:prevState.to,from:e.target.value}})
+  };
 
-  const handleTo = (e) =>{
-    setTo(e.target.value);
-  }  
+  const handleTo = (e) => {
 
+    setCurrency((prevState)=>{return {to:e.target.value,from:prevState.from}})
+  };
 
-  return<div className="main-parent">
-    <h2 className="header">Crimson Currency Converter</h2>
-    <div className="table">
-      <Converter handleAmount = {handleAmount} handleFrom = {handleFrom} handleTo = {handleTo} from = {from} to = {to} swap = {swap} amount = {amount}/>
-      {APIDone?<Result converted = {converted} amount = {finalFinalAmount} from = {finalFinalFrom} to = {finalFinalTo}/>:<></>} 
-      <Submit loading = {loading} Click = {Click}/>
+  return (
+    <div className="main-parent">
+      <h2 className="header">Crimson Currency Converter</h2>
+      <div className="table">
+        <Converter 
+          handleAmount={handleAmount}
+          handleFrom={handleFrom}
+          handleTo={handleTo}
+          from={currency.from}
+          to={currency.to}
+          swap={swap}
+          amount={amount}
+        />
+        {APIDone && start? (
+          <Result
+            converted={converted}
+            amount={amount}
+            from={currency.from}
+            to={currency.to}
+          />
+        ) : (
+          <></>
+        )}
+        <Submit loading={loading} Click={Click} />
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
-
-
-
-  export default App;
+export default App;
--- src/converter.js
+++ src/converter.js
@@ -1,52 +1,79 @@
-import "../src/App.css"
-import React, { useState } from "react"
+import "../src/App.css";
+import React, { useState } from "react";
 import { useEffect } from "react";
 
-
-const Converter = (props) =>{ 
-  const {handleAmount,handleFrom,handleTo,from,to,swap,amount} = props;
-  const [curList,setCurList] = useState([])
-  const [fullCurList,setFullCurList] = useState([])
-  useEffect(()=>{
-    const host = 'api.frankfurter.app'
+const Converter = (props) => {
+  const { handleAmount, handleFrom, handleTo, from, to, swap, amount} = props;
+  const [curList, setCurList] = useState([]);
+  const [fullCurList, setFullCurList] = useState([]);
+  useEffect(() => {
+    const host = "api.frankfurter.app";
     fetch(`https://${host}/currencies`)
-    .then(resp => resp.json())
-    .then(data => {
-      setFullCurList(data)
-      setCurList(Object.keys(data))
-    })
-    },[])
-  
-  return <div className="converter">
-    <div className="amount">
-      <h4>Amount</h4>
-        <input type = "text" value = {amount} onSubmit={(e) => {e.preventDefault()}} onChange={handleAmount}/>
-    </div>
-    <div className="from">
+      .then((resp) => resp.json())
+      .then((data) => {
+        setFullCurList(data);
+        setCurList(Object.keys(data));
+      });
+  }, []);
+
+  return (
+    <div className="converter">
+      <div className="amount">
+        <h4>Amount</h4>
+        <input
+          type="text"
+          value={amount}
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          onChange={handleAmount}
+        />
+      </div>
+      <div className="from">
         <h4>From</h4>
-      <select value = {from} onSubmit={(e) => {e.preventDefault()}} onChange={handleFrom}>
-        {curList.map((cur)=>{
-          return <>
-            <option value = {cur}>{cur} - {fullCurList[cur]} </option>
-          </>
-        })}
-      </select>
-    </div>
-    <div className="swap">
-        <button onClick={swap}>↔️</button>
-    </div>
-    <div className="to">
-      <h4>To</h4>
-        <select value= {to}  onChange={handleTo} onSubmit={(e) => {e.preventDefault()}}>
-          {curList.map((cur)=>{
-            return <>
-              <option value = {cur}>{cur} - {fullCurList[cur]} </option>
-            </>
+        <select
+          value={from}
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          onChange={handleFrom}
+        >
+          {curList.map((cur) => {
+            return (
+              <>
+                <option value={cur}>
+                  {cur} - {fullCurList[cur]}{" "}
+                </option>
+              </>
+            );
           })}
         </select>
+      </div>
+      <div className="swap">
+        <button onClick={swap}>↔️</button>
+      </div>
+      <div className="to">
+        <h4>To</h4>
+        <select
+          value={to}
+          onChange={handleTo}
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          {curList.map((cur) => {
+            return (
+              <>
+                <option value={cur}>
+                  {cur} - {fullCurList[cur]}{" "}
+                </option>
+              </>
+            );
+          })}
+        </select>
+      </div>
     </div>
-</div>
-}
+  );
+};
 
-
-export default Converter;
+export default Converter
